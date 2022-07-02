5942d24516d59f2ca85b2e80be2c17441ba58272
Ratnajit Sarkar <ratnajit.ss@gmail.com>
2022-07-02 18:10:16 +0800

    stylise Account Information screen

--- components/AccountInfo.js
+++ components/AccountInfo.js
@@ -4,10 +4,12 @@ import styles from './AccountInfo.module.scss'
 const AccountInfo = ({ accountInfo }) => {
   return (
     <div className={styles.container}>
-        <h1 className={styles.accountName}>{accountInfo.name}</h1>
-        <span className={styles.token}>{accountInfo.token}</span>
-
-        <h1 className={styles.accountBalance}>$ {accountInfo.balance}</h1>
+        <div>
+            <h1 className={styles.accountName}>{accountInfo.name}</h1>
+            <span className={styles.token}>{accountInfo.token}</span>
+            <h1 className={styles.accountBalance}>$ {accountInfo.balance}</h1>
+        </div>
+        <button className={styles.paymentButton}>Make Payment</button>
     </div>
   )
 }
